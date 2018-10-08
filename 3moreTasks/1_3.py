daf8b5a477e7fd7846e0b3bd507a15ee221e0bfc
Sophia Kramar <krasofija@yandex.ru>
2018-10-08 15:54:08 +0300

    Cкрипт, позволяющий выполнять импорт файлов по URL

--- 3moreTasks/1_3.py
+++ 3moreTasks/1_3.py
@@ -1,5 +1,5 @@
 import requests #made with my fav lib
-
+ 
 url = 'https://www.python.org/static/img/python-logo.png'
 fileName = '/Users/vonkuptschino/Desktop/УЧЁБИЩЕ/2 курс/pythonist wannabe/5sem/PythonLogo.png'
 req = requests.get(url)
